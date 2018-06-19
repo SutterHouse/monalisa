9aa8e6542a4af5ded14e4991e8b97ccfa0b35097
William Trousdale <trousdalew@gmail.com>
2018-06-18 20:08:35 -0700

    Removes test server

--- spec/compareImages/server.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-
-const PORT = 3030;
-app.listen(PORT, () => {
-  console.log('Listening on port ' + PORT);
-});
-
-app.use(express.static(__dirname));

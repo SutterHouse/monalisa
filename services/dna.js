a3e5f23babe5d7b185eec9422f81329ca208e1ad
William Trousdale <trousdalew@gmail.com>
2018-06-18 22:16:26 -0700

    Adds mutate method to DNA class

--- services/dna.js
+++ services/dna.js
@@ -5,6 +5,16 @@ class DNA {
     this.canvasHeight = canvasHeight;
     this.averagePolygonVertices = 5;
 
+    this.LO_COORD_MUT_LB = .95;
+    this.LO_COORD_MUT_UB = 1.05;
+    this.HI_COORD_MUT_LB = .90;
+    this.HI_COORD_MUT_UB = 1.10;
+    
+    this.LO_COLOR_MUT_LB = .95;
+    this.LO_COLOR_MUT_UB = 1.05;
+    this.HI_COLOR_MUT_LB = .90;
+    this.HI_COLOR_MUT_UB = 1.10;
+
     this.polygons = [];
 
     for (var i = 0; i < this.numberOfPolygons; i++) {
@@ -29,9 +39,51 @@ class DNA {
       this.polygons.push(polygon);
     }
   }
+  //Edge cases to address:
+  //  x or y mutation can go past image boundary
+  //  0 values cannot be mutated
+  mutate() {
+    this.polygons.forEach((polygon) => {
+      polygon.coordinates.forEach((coordinate) => {
+        let px = Math.floor(Math.random() * (3 + 1));
+        let py = Math.floor(Math.random() * (3 + 1));
 
+        if (px === 2) {
+          coordinate.x *= Math.random() * (this.LO_COORD_MUT_UB - this.LO_COORD_MUT_LB) + this.LO_COORD_MUT_LB;
+        } else if (px === 3) {
+          coordinate.x *= Math.random() * (this.HI_COORD_MUT_UB - this.HI_COORD_MUT_LB) + this.HI_COORD_MUT_LB;
+        }
+        if (py === 2) {
+          coordinate.y *= Math.random() * (this.LO_COORD_MUT_UB - this.LO_COORD_MUT_LB) + this.LO_COORD_MUT_LB;
+        } else if (py === 3) {
+          coordinate.y *= Math.random() * (this.HI_COORD_MUT_UB - this.HI_COORD_MUT_LB) + this.HI_COORD_MUT_LB;
+        }
+      });
 
+      let pr = Math.floor(Math.random() * (3 + 1));
+      let pg = Math.floor(Math.random() * (3 + 1));
+      let pb = Math.floor(Math.random() * (3 + 1));
+      
+      if (pr === 2) {
+        polygon.color.r *= Math.random() * (this.LO_COLOR_MUT_UB - this.LO_COLOR_MUT_LB) + this.LO_COLOR_MUT_LB;
+      } else if (pr === 3) {
+        polygon.color.r *= Math.random() * (this.HI_COLOR_MUT_UB - this.HI_COLOR_MUT_LB) + this.HI_COLOR_MUT_LB;
+      }
+      if (pg === 2) {
+        polygon.color.g *= Math.random() * (this.LO_COLOR_MUT_UB - this.LO_COLOR_MUT_LB) + this.LO_COLOR_MUT_LB;
+      } else if (pg === 3) {
+        polygon.color.g *= Math.random() * (this.HI_COLOR_MUT_UB - this.HI_COLOR_MUT_LB) + this.HI_COLOR_MUT_LB;
+      }
+      if (pb === 2) {
+        polygon.color.b *= Math.random() * (this.LO_COLOR_MUT_UB - this.LO_COLOR_MUT_LB) + this.LO_COLOR_MUT_LB;
+      } else if (pb === 3) {
+        polygon.color.b *= Math.random() * (this.HI_COLOR_MUT_UB - this.HI_COLOR_MUT_LB) + this.HI_COLOR_MUT_LB;
+      }
+    });
+  }
 }
 
 var dna = new DNA(50, 200, 200);
+console.log(JSON.stringify(dna));
+dna.mutate();
 console.log(JSON.stringify(dna));
